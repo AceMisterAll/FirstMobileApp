8c4cf6453bd1625992dd175a2fb6fa98152fc15f
Allan Chiron <allanloannchiron@gmail.com>
2025-12-10 17:55:33 +0100

    Initial Commit

---
+++ FirstMobileApp/FirstMobileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstMobileAppApp.swift
+//  FirstMobileApp
+//
+//  Created by Allan Chiron on 10/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct FirstMobileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
